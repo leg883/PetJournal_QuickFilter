3b6705d4f1babbbf1c5f19cca40c0f8081844f9d
leg883 <leg883@126.com>
2015-07-09 02:00:08 +0800

    Update PetJournal_QuickFilter.lua

--- PetJournal_QuickFilter.lua
+++ PetJournal_QuickFilter.lua
@@ -12,9 +12,9 @@ local QuickFilter_Function = function(self, button)
     for petType, _ in ipairs(PET_TYPE_SUFFIX) do
         local btn = _G["PetJournalQuickFilterButton"..petType]
         if "LeftButton" == button then
-            if self == btn and (not btn.isActive) then
-                btn.isActive = true
-            else
+            if self == btn then
+                btn.isActive = not btn.isActive
+            elseif not IsShiftKeyDown() then
                 btn.isActive = false
             end
         elseif "RightButton" == button and (self == btn) then
@@ -45,11 +45,13 @@ local QuickFilter_Function = function(self, button)
     end
 end
 
--- Create the pet type buttons
-for petType, suffix in ipairs(PET_TYPE_SUFFIX) do
-    local btn = CreateFrame("Button", "PetJournalQuickFilterButton"..petType, PetJournalLeftInset)
+-- Create the pet type buttons, sorted according weakness
+-- Humanoid > Dragonkin > Magic > Flying > Aquatic > Elemental > Mechanical > Beast > Critter > Undead
+local activeCount = 0
+for petIndex, petType in ipairs({1, 2, 6, 3, 9, 7, 10, 8, 5, 4}) do
+    local btn = CreateFrame("Button", "PetJournalQuickFilterButton"..petIndex, PetJournal)
     btn:SetSize(24, 24)
-    btn:SetPoint("TOPLEFT", PetJournalLeftInset, 6 + 25 * (petType-1), -33)
+    btn:SetPoint("TOPLEFT", PetJournalLeftInset, 6 + 25 * (petIndex-1), -33)
     
     local background = btn:CreateTexture(nil, "BACKGROUND")
     background:SetTexture("Interface\\PetBattles\\PetBattleHud")
@@ -59,7 +61,7 @@ for petType, suffix in ipairs(PET_TYPE_SUFFIX) do
     btn.Background = background
     
     local icon = btn:CreateTexture(nil, "ARTWORK")
-    icon:SetTexture("Interface\\PetBattles\\PetIcon-"..suffix)
+    icon:SetTexture("Interface\\PetBattles\\PetIcon-"..PET_TYPE_SUFFIX[petType])
     icon:SetTexCoord(0.79687500, 0.49218750, 0.50390625, 0.65625000)
     icon:SetSize(22, 22)
     icon:SetPoint("CENTER")
@@ -72,8 +74,22 @@ for petType, suffix in ipairs(PET_TYPE_SUFFIX) do
     highlight:SetPoint("CENTER")
     btn:SetHighlightTexture(highlight, "BLEND")
     
-    btn.isActive = false
+    if not C_PetJournal.IsPetTypeFiltered(petType) then
+        btn.isActive = true
+        btn:LockHighlight()
+        activeCount = activeCount + 1
+    else
+        btn.isActive = false
+    end
     btn.petType = petType
     
     btn:SetScript("OnMouseUp", QuickFilter_Function)
+end
+
+if #PET_TYPE_SUFFIX == activeCount then
+    for petIndex, _ in ipairs(PET_TYPE_SUFFIX) do
+        local btn = _G["PetJournalQuickFilterButton"..petIndex]
+        btn.isActive = false
+        btn:UnlockHighlight()
+    end
 end
